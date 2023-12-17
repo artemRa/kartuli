7057a6e76b3e72da7a4491e12b0935bf797c3746
artemRa <aaartem.ra@gmail.com>
2023-12-17 22:03:50 +0400

    invisible

--- query.sql
+++ query.sql
@@ -1,5 +1,18 @@
 -- !preview conn=DBI::dbConnect(RSQLite::SQLite(), dbname = "kartuli.db")
 
+/*
 SELECT sid, count(*) 
 FROM ka_sentences
 GROUP BY sid
+
+select count(*) 
+from ka_sentences
+--where sid = 23
+--order by random()
+
+select count(*), 
+from ka_words
+*/ 
+
+select count(*), count(distinct wrd)
+from ka_words 
